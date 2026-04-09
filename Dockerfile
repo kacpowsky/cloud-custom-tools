2cec89527a611d3b584077b73fdc9b5e808cb9c4
kacpowsky <nedry@0eov04vh-osx.tail8b71ac.ts.net>
2026-04-09 12:40:33 +0200

    2026-04-09 12:40:33 |  | ID:B4B91B71

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM python:alpine
+
+# Set environment variables
+# ENV KUBECTL_VERSION=$(curl -s https://dl.k8s.io/release/stable.txt)
+
+ADD run.sh /run.sh
+
+# Install required packages
+RUN apk add --no-cache \
+    bash \
+    curl \
+    jq \
+    netcat-openbsd
+
+RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl \
+	&& mv kubectl /usr/local/bin \
+	&& chmod +x /usr/local/bin/kubectl
+
+RUN curl -fsSL https://raw.githubusercontent.com/scaleway/scaleway-cli/master/scripts/get.sh | sh
+
+
+# RUN curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl" \
+#     && chmod +x kubectl \
+#     && mv kubectl /usr/local/bin/kubectl
+
+    
+RUN adduser -S user
+USER user
+WORKDIR /home/user
+ENV PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/user/.local/bin
+
+RUN pip install awscli --upgrade --user
---
+++ run.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+set -e
+
+
+echo "Setting up Scaleway CLI config"
+# Scaleway CLI config (expects env vars)
+: "${SCW_ACCESS_KEY:?missing}"
+: "${SCW_SECRET_KEY:?missing}"
+: "${SCW_DEFAULT_PROJECT_ID:?missing}"
+: "${SCW_DEFAULT_REGION:?missing}"   # e.g. fr-par
+: "${SCW_DEFAULT_ZONE:?missing}"     # e.g. fr-par-1
+
+SCW_CONFIG_DIR="${HOME}/.config/scw"
+SCW_CONFIG_FILE="${SCW_CONFIG_DIR}/config.yaml"
+
+mkdir -p "${SCW_CONFIG_DIR}"
+chmod 700 "${SCW_CONFIG_DIR}"
+
+umask 077
+cat > "${SCW_CONFIG_FILE}" <<EOF
+access_key: ${SCW_ACCESS_KEY}
+secret_key: ${SCW_SECRET_KEY}
+default_project_id: ${SCW_DEFAULT_PROJECT_ID}
+default_region: ${SCW_DEFAULT_REGION}
+default_zone: ${SCW_DEFAULT_ZONE}
+EOF
+
+
+
+echo "Retrieving Docker Credentials for the AWS ECR Registry ${AWS_ACCOUNT}"
+DOCKER_REGISTRY_SERVER=https://${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com
+DOCKER_USER=AWS
+DOCKER_PASSWORD=$(aws ecr get-login-password --region ${AWS_REGION})
+
+IFS=';' read -r -a SUFFIXES <<< "$NAMESPACE_SUFFIX"
+
+for suffix in "${SUFFIXES[@]}"; do
+  echo "Processing namespaces with suffix: ${suffix}"
+
+  while read namespace
+  do
+    echo ${namespace}
+
+    echo "Removing previous secret in namespace ${namespace}"
+    kubectl --context ${KUBE_CONTEXT} --namespace=${namespace} delete secret aws-ecr-credentials || true
+
+    echo "Creating new secret in namespace ${namespace}"
+    kubectl --context ${KUBE_CONTEXT} --namespace=${namespace} create secret docker-registry aws-ecr-credentials \
+      --docker-server=${DOCKER_REGISTRY_SERVER} \
+      --docker-username=${DOCKER_USER} \
+      --docker-password=${DOCKER_PASSWORD} \
+      --docker-email=srv+ecr@starbeta.net
+
+  # done < <(kubectl get ns --no-headers -o custom-columns=":metadata.name" | grep ${suffix})
+  done < <(kubectl --context ${KUBE_CONTEXT} get ns --no-headers -o custom-columns=":metadata.name" | grep ${suffix})
+
+done
